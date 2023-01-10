d04b9ce69909cfcb79c503e66b29dd140ab257b0
Ghulam Subbhani <gsmalik030@gmail.com>
2023-01-10 05:48:17 +0200

    _Created Book List

--- script.js
+++ script.js
@@ -5,22 +5,67 @@ class Awesome {
     let book = {
       title,
       author,
-      isbn: Date.now(),
+      id: Math.round(Math.random() * 1000000000),
     };
-    this.books.unshift(book);
+    this.books.push(book);
     const allBooks = JSON.stringify(this.books);
     localStorage.setItem("books", allBooks);
   }
 
-  remove(isbn) {
-    this.books = this.books = this.books.filter((x) => x.isbn !== isbn);
+  remove(id) {
+    this.books = this.books = this.books.filter((x) => x.id !== id);
     const allBooks = JSON.stringify(this.books);
     localStorage.setItem("books", allBooks);
+  }
+
+  stored() {
+    this.books = JSON.parse(localStorage.getItem("books"));
   }
 }
 
-let book1 = new Awesome();
+let addBook = new Awesome();
 
-console.log(book1.books);
+const bookDisplay = document.getElementById("book-display");
+const form = document.getElementById("form");
 
+function createBook() {
+  if (addBook.books.length === 0) {
+    const noBookheading = document.createElement("h2");
+    const noBookText = document.createTextNode("Book List is empty");
+    noBookheading.appendChild(noBookText);
+    bookDisplay.append(noBookheading);
+  } else {
+    const bookUl = document.createElement("ul");
+    bookUl.classList.add("book-ul");
+    addBook.books.map((y) => {
+      const bookLi = document.createElement("li");
+      bookLi.classList.add("book-list");
+      const titleHeading = document.createElement("h3");
+      const titleTxt = document.createTextNode(`"${y.title}" By ${y.author}`);
+      titleHeading.appendChild(titleTxt);
+      bookLi.appendChild(titleHeading);
+      const btnRemove = document.createElement("button");
+      const btnTxt = document.createTextNode("Remove");
+      btnRemove.onclick = function () {
+        addBook.remove(y.id);
+        location.reload();
+      };
+      btnRemove.appendChild(btnTxt);
+      bookLi.appendChild(btnRemove);
+      bookUl.appendChild(bookLi);
+
+      return bookLi;
+    });
+    bookDisplay.appendChild(bookUl);
+  }
+}
+function bookAdded() {
+  let bookTitle = document.getElementById("book-title").value;
+  let authorName = document.getElementById("author-name").value;
+  addBook.add(bookTitle, authorName);
+  location.reload();
+}
+addBook.stored();
+createBook();
+form.addEventListener("submit", bookAdded);
 
