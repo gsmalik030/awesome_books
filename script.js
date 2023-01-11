047602426c15a0017f4ddb0d136430a4162d52e1
Ghulam Subbhani <gsmalik030@gmail.com>
2023-01-11 16:30:29 +0200

    _Fixed Eslint errors

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-const addButton = document.getElementsByClassName("add-btn")[0];
-const addedSection = document.getElementsByClassName("books")[0];
+const addButton = document.getElementsByClassName('add-btn')[0];
+const addedSection = document.getElementsByClassName('books')[0];
 let books;
 
-addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const title = document.getElementById("title").value.trim();
-  const author = document.getElementById("author").value.trim();
-  addBooks(title, author);
-  display();
-});
-
-const addBooks = (Title, Author) => {
-  if (Title !== "" && Author !== "") {
-    var obj = {
-      title: Title,
-      author: Author,
-    };
-    books.push(obj);
-    localStorage.setItem("Books", JSON.stringify(books));
-  }
-};
-
-window.addEventListener("DOMContentLoaded", () => {
-  display();
-});
-
 function display() {
-  if (localStorage.getItem("Books") == null) {
+  if (localStorage.getItem('Books') == null) {
     books = [];
   } else {
-    books = JSON.parse(localStorage.getItem("Books"));
+    books = JSON.parse(localStorage.getItem('Books'));
   }
 
-  let display = ``;
+  let displayed = '';
   books.forEach((addedSection, i) => {
-    display += `
+    displayed += `
         <div>
         <p>${addedSection.title}</p>
         <p>${addedSection.author}</p>
@@ -43,17 +20,41 @@ function display() {
         </div>
         `;
   });
-  addedSection.innerHTML = display;
+  addedSection.innerHTML = displayed;
 }
 
+const addBooks = (Title, Author) => {
+  if (Title !== '' && Author !== '') {
+    const obj = {
+      title: Title,
+      author: Author,
+    };
+    books.push(obj);
+    localStorage.setItem('Books', JSON.stringify(books));
+  }
+};
+
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value.trim();
+  const author = document.getElementById('author').value.trim();
+  addBooks(title, author);
+  display();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  display();
+});
+
+// eslint-disable-next-line no-unused-vars
 const removeBook = (id) => {
-  if (localStorage.getItem("Books") == null) {
+  if (localStorage.getItem('Books') == null) {
     books = [];
   } else {
-    books = JSON.parse(localStorage.getItem("Books"));
+    books = JSON.parse(localStorage.getItem('Books'));
   }
   const bookIndex = books.findIndex((item, i) => i === id);
   books.splice(bookIndex, 1);
-  localStorage.setItem("Books", JSON.stringify(books));
+  localStorage.setItem('Books', JSON.stringify(books));
   display();
 };
