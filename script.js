263f624ca1350067e8eaf85a032d4a86ed81a188
Ghulam Subbhani <gsmalik030@gmail.com>
2023-01-12 14:44:09 +0200

    _Fixed linters

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const storageKey = "awesomeBooks";
-const currentFormTitle = document.querySelector("#title");
-const currentFormAuthor = document.querySelector("#author");
-const currentFormBookAddButton = document.querySelector("#book-add-button");
-const linkAdd = document.querySelector("#link-add");
-const linkList = document.querySelector("#link-list");
-const linkContact = document.querySelector("#link-contact");
-const books = document.querySelector(".book-shelf");
-const form = document.querySelector(".form");
-const contact = document.querySelector(".contact");
-const dateTime = document.querySelector(".date-time");
+const storageKey = 'awesomeBooks';
+const currentFormTitle = document.querySelector('#title');
+const currentFormAuthor = document.querySelector('#author');
+const currentFormBookAddButton = document.querySelector('#book-add-button');
+const linkAdd = document.querySelector('#link-add');
+const linkList = document.querySelector('#link-list');
+const linkContact = document.querySelector('#link-contact');
+const books = document.querySelector('.book-shelf');
+const form = document.querySelector('.form');
+const contact = document.querySelector('.contact');
+const dateTime = document.querySelector('.date-time');
 const timeNow = new Date();
 
 class BookShelf {
@@ -42,20 +42,20 @@ class BookShelf {
       return element;
     }
 
-    const bookList = document.querySelector(".book-list");
+    const bookList = document.querySelector('.book-list');
     this.arrBooks.forEach((book) => {
-      const bookItem = addElement("div", bookList, "book-item");
-      const bookTitle = addElement("div", bookItem, "book-title");
+      const bookItem = addElement('div', bookList, 'book-item');
+      const bookTitle = addElement('div', bookItem, 'book-title');
       bookTitle.innerHTML = `" ${book.title} "    by ${book.author}`;
 
       const bookRemoveButton = addElement(
-        "button",
+        'button',
         bookItem,
-        "book-remove-button"
+        'book-remove-button',
       );
-      bookRemoveButton.innerHTML = "Remove";
+      bookRemoveButton.innerHTML = 'Remove';
 
-      bookRemoveButton.addEventListener("click", () => {
+      bookRemoveButton.addEventListener('click', () => {
         this.removeBook(book.id);
         this.saveDataToLocalStorage();
         window.location.reload();
@@ -77,35 +77,35 @@ class BookShelf {
 
 const awesomeBookShelf = new BookShelf();
 
-currentFormBookAddButton.addEventListener("click", () => {
+currentFormBookAddButton.addEventListener('click', () => {
   const title = currentFormTitle.value;
   const author = currentFormAuthor.value;
   awesomeBookShelf.addBook(title, author);
   awesomeBookShelf.saveDataToLocalStorage();
 });
 
-linkList.addEventListener("click", () => {
-  books.classList.remove("hide");
-  form.classList.add("hide");
-  contact.classList.add("hide");
+linkList.addEventListener('click', () => {
+  books.classList.remove('hide');
+  form.classList.add('hide');
+  contact.classList.add('hide');
 });
 
-linkAdd.addEventListener("click", () => {
-  books.classList.add("hide");
-  form.classList.remove("hide");
-  contact.classList.add("hide");
+linkAdd.addEventListener('click', () => {
+  books.classList.add('hide');
+  form.classList.remove('hide');
+  contact.classList.add('hide');
 });
 
-linkContact.addEventListener("click", () => {
-  books.classList.add("hide");
-  form.classList.add("hide");
-  contact.classList.remove("hide");
+linkContact.addEventListener('click', () => {
+  books.classList.add('hide');
+  form.classList.add('hide');
+  contact.classList.remove('hide');
 });
 
 window.onload = () => {
-  books.classList.remove("hide");
-  form.classList.add("hide");
-  contact.classList.add("hide");
+  books.classList.remove('hide');
+  form.classList.add('hide');
+  contact.classList.add('hide');
   awesomeBookShelf.loadDataFromLocalStorage();
   dateTime.innerHTML = timeNow;
 };
